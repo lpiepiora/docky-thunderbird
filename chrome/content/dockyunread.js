3dac69d0b13af69ff0f8d53ab942a57e7f7159e1
lpiepiora <lpiepiora@octopus.(none)>
2010-02-05 12:33:15 +0100

    Changed the way the thunderbird docky item is selected, converted spaces to tabs

--- chrome/content/dockyunread.js
+++ chrome/content/dockyunread.js
@@ -5,68 +5,68 @@ var dockyunread = {
 	},
 	
 	onClose: function(e) {
-    	this.initialized = true;
-	    this.resetUnreadCount();
+		this.initialized = true;
+		this.resetUnreadCount();
 	},
 	
 	resetUnreadCount: function() {
-	    this.updateUnreadCount(0, true);
+		this.updateUnreadCount(0, true);
 	},
 	
-    updateUnreadCount: function(x, blockingProcess){
-        try {
-            netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
-        } catch (e) {
-            alert(noReading);
-        }
-        var file = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsILocalFile);
-        
-        const DIR_SERVICE = new Components.Constructor("@mozilla.org/file/directory_service;1","nsIProperties");
-        try { 
-            path=(new DIR_SERVICE()).get("ProfD", Components.interfaces.nsIFile).path; 
-        } catch (e) {
-            alert(error);
-        }
-        
-        path = path + "/extensions/docky-unread@lpiepiora.com/chrome/content/update-badge.py";
-        file.initWithPath(path);
-        
-        var args = [x];
-        var process = Components.classes["@mozilla.org/process/util;1"].createInstance(Components.interfaces.nsIProcess);
-        process.init(file);
-        process.run(blockingProcess, args, args.length);
-    },
+	updateUnreadCount: function(x, blockingProcess){
+		try {
+			netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
+		} catch (e) {
+			alert(noReading);
+		}
+		var file = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsILocalFile);
+		
+		const DIR_SERVICE = new Components.Constructor("@mozilla.org/file/directory_service;1","nsIProperties");
+		try { 
+			path=(new DIR_SERVICE()).get("ProfD", Components.interfaces.nsIFile).path; 
+		} catch (e) {
+			alert(error);
+		}
+		
+		path = path + "/extensions/docky-unread@lpiepiora.com/chrome/content/update-badge.py";
+		file.initWithPath(path);
+		
+		var args = [x];
+		var process = Components.classes["@mozilla.org/process/util;1"].createInstance(Components.interfaces.nsIProcess);
+		process.init(file);
+		process.run(blockingProcess, args, args.length);
+	},
 
 	onItemCountChanged : function() {
-	    if (this.timeoutId != -1) {
-	        window.clearTimeout(this.timeoutId);
-	    }
-	    // Schedule on the main thread
+		if (this.timeoutId != -1) {
+			window.clearTimeout(this.timeoutId);
+		}
+		// Schedule on the main thread
 		this.timeoutId = window.setTimeout(this.performUnreadCount, 1000, this);
-    },
-    
-    performUnreadCount: function(that) {
-        const MSG_FOLDER_FLAG_INBOX = 0x1000;
-        var acctMgr = Components.classes["@mozilla.org/messenger/account-manager;1"].getService(Components.interfaces.nsIMsgAccountManager);  
-        var accounts = acctMgr.accounts;  
+	},
+	
+	performUnreadCount: function(that) {
+		const MSG_FOLDER_FLAG_INBOX = 0x1000;
+		var acctMgr = Components.classes["@mozilla.org/messenger/account-manager;1"].getService(Components.interfaces.nsIMsgAccountManager);  
+		var accounts = acctMgr.accounts;  
 		var totalCount = 0;
-        for (var i = 0; i < accounts.Count(); i++) {
-            var account = accounts.QueryElementAt(i, Components.interfaces.nsIMsgAccount);  
-            var rootFolder = account.incomingServer.rootFolder; // nsIMsgFolder            
-            if (rootFolder.hasSubFolders) {
-                var subFolders = rootFolder.subFolders; // nsIMsgFolder
-                while(subFolders.hasMoreElements()) {
-                    var folder = subFolders.getNext().QueryInterface(Components.interfaces.nsIMsgFolder);
-                    if(folder.flags & MSG_FOLDER_FLAG_INBOX) {
-                        totalCount += folder.getNumUnread(false);
-                    }
-                }
-            }
-       }
-       that.updateUnreadCount(totalCount, false);
-    },    
-    
-    folderListener : {
+		for (var i = 0; i < accounts.Count(); i++) {
+			var account = accounts.QueryElementAt(i, Components.interfaces.nsIMsgAccount);  
+			var rootFolder = account.incomingServer.rootFolder; // nsIMsgFolder            
+			if (rootFolder.hasSubFolders) {
+				var subFolders = rootFolder.subFolders; // nsIMsgFolder
+				while(subFolders.hasMoreElements()) {
+					var folder = subFolders.getNext().QueryInterface(Components.interfaces.nsIMsgFolder);
+					if(folder.flags & MSG_FOLDER_FLAG_INBOX) {
+						totalCount += folder.getNumUnread(false);
+					}
+				}
+			}
+	}
+	that.updateUnreadCount(totalCount, false);
+	},    
+	
+	folderListener : {
 		OnItemAdded : function(parent, item, viewString) {
 				dockyunread.onItemCountChanged();
 		},
@@ -74,9 +74,9 @@ var dockyunread = {
 				dockyunread.onItemCountChanged();
 		},
 		OnItemPropertyFlagChanged : function(item, property, oldFlag, newFlag) {
-		    if (property=="Status"){
-                dockyunread.onItemCountChanged();
-            }
+			if (property=="Status"){
+				dockyunread.onItemCountChanged();
+			}
 		},
 		OnItemEvent : function(item, event) {
 				dockyunread.onItemCountChanged();
@@ -89,9 +89,9 @@ var dockyunread = {
 		OnItemBoolPropertyChanged : function(item, property, oldValue, newValue) {},
 		OnItemUnicharPropertyChanged : function(item, property, oldValue, newValue) {}
 	},
-    mailSession: '',
-    notifyFlags: '',
-    timeoutId: -1
+	mailSession: '',
+	notifyFlags: '',
+	timeoutId: -1
 };
 
 window.addEventListener("load", function(e) { dockyunread.onLoad(e); }, false);
--- chrome/content/update-badge.py
+++ chrome/content/update-badge.py
@@ -4,20 +4,25 @@ import sys
 import dbus
 import os
 
+def toDockyItem(path):
+	itemObj = bus.get_object("org.gnome.Docky", path)
+	itemInt = dbus.Interface(itemObj, "org.gnome.Docky.Item")
+	return itemInt
+
 bus = dbus.SessionBus()
 obj = bus.get_object("org.gnome.Docky", "/org/gnome/Docky")
 docky = dbus.Interface(obj, "org.gnome.Docky")
 
 if len(sys.argv) < 2:
-    unread_count = 0
+	unread_count = 0
 else:
-    unread_count = int(sys.argv[1])
+	unread_count = int(sys.argv[1])
 
-for path in docky.DockItemPaths():
-    itemObj = bus.get_object("org.gnome.Docky", path)
-    itemInt = dbus.Interface(itemObj, "org.gnome.Docky.Item")
-    if (itemInt.GetName().lower().find("thunderbird") != -1):
-        if (unread_count > 0):
-            itemInt.SetBadgeText("%s" % unread_count)
-        else:
-            itemInt.ResetBadgeText()
+dockyItems = [toDockyItem(path) for path in docky.DockItemPaths()]
+
+for item in dockyItems:
+	if (item.GetOwnsDesktopFile() and item.GetDesktopFile().endswith("thunderbird.desktop")):
+		if (unread_count > 0):
+			item.SetBadgeText("%s" % unread_count)
+		else:
+			item.ResetBadgeText()
