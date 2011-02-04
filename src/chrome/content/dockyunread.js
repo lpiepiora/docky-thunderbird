1ed6b77b34ee85bce8a3ba4302c2c3721bd038bd
Timm Preetz <timm@preetz.us>
2011-02-04 18:09:14 +0100

    Use Python 2.x specifically

--- src/chrome/content/dockyunread.js
+++ src/chrome/content/dockyunread.js
@@ -47,7 +47,7 @@ var dockyunread = {
 		var file = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsILocalFile);		
 		file.initWithPath("/usr/bin/env");
 		
-		var args = ["python", path, x];
+		var args = ["python2", path, x];
 		var process = Components.classes["@mozilla.org/process/util;1"].createInstance(Components.interfaces.nsIProcess);
 		process.init(file);
 		
